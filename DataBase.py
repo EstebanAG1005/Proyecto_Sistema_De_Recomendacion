c6291a9609df315a9738ac6c855d0a39e4242b45
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-05-21 18:08:04 -0600

    Main

--- DataBase.py
+++ DataBase.py
@@ -4,3 +4,5 @@ from neo4j import GraphDatabase
 db = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "1234"), encrypted=False)
 
 session = db.session()
+
+
---
+++ Main.py
@@ -0,0 +1,15 @@
+
+run = True
+
+while run:
+    print("Sistema de Recomendacion de Restaurantes")
+    print("1. Realizar una recomendacion\n2.Salir del Programa")
+    op = input("Ingrese la opcion a realizar: ")
+
+    # Empezar el con el sistema de recomendacion
+    if op == "1":
+        print("Procesando opciones\n")
+
+    if op == "2":
+        print("Gracias por utlizar el programa :)\n ")
+        run = False
