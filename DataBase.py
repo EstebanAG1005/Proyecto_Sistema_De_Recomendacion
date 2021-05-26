175ea3bb559b73f4fc21b2c28a96fa0c49f21a9c
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-05-26 16:59:37 -0600

    Se agregan datos a la base de datos

--- DataBase.py
+++ DataBase.py
@@ -1,12 +1,27 @@
+# Generar los Restaurante
+import numpy as np
+import random
+import pandas as pd
 from neo4j import GraphDatabase
 
-db = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "1234"))
-session=db.session()
+df = pd.read_csv("export.csv")
 
-def get_person():
-    query="MATCH (n:Person) RETURN n LIMIT 25"
-    nodes = session.run(query)
-    nodes = list(nodes)
-    print(nodes)
+transaction_list = df.values.tolist()
 
-get_person()
+transaction_execution_commands = []
+
+for i in transaction_list:
+    neo4j_create_statemenet = "create (n:Restaurante {Nombre: '" + str(i[0]) + "', Especialidad: '" + str(i[1]) + "'})"
+    transaction_execution_commands.append(neo4j_create_statemenet)
+
+
+def execute_transactions(transaction_execution_commands):
+    data_base_connection = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "12345"))
+    session = data_base_connection.session()
+    for i in transaction_execution_commands:
+        session.run(i)
+
+
+execute_transactions(transaction_execution_commands)
+
+execute_transactions(transaction_execution_commands)
