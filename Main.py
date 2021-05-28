ce0a79ee65841758904732096d2e289ba02fa379
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2021-05-27 22:40:27 -0600

    Update Main.py
    Menu con todas las preguntas

--- Main.py
+++ Main.py
@@ -41,15 +41,322 @@ while not run:
           if op1 == 1:
             print("--Especialidad Pizza--")
             run1 = True
+
+            print("")
+            print("¿Qué nivel de precios relativos estaria dispuesto a pagar?")
+            print("1. Alto (Q.71+) \n2. Medio (Q.51 - Q.70) \n3. Bajo(Q.1 - Q.50)")
+            run2 = False
+            while not run2:
+              op2 = pedirNumeroEntero()
+
+              if op2 == 1:
+                print("--Especialidad Pizza, Precio Alto--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Pizza, Precio Alto, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Pizza, Precio Alto, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+                    
+
+              elif op2 == 2:
+                print("--Especialidad Pizza, Precio Medio--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Pizza, Precio Medio, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Pizza, Precio Medio, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+              elif op2 == 3:
+                print("--Especialidad Pizza, Precio Bajo--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Pizza, Precio Bajo, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  if op3 == 2:
+                    print("--Especialidad Pizza, Precio Bajo, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+
+              else:
+                print("\n Ingrese un numero entre 1 - 3 \n")
+
+
           elif op1 == 2:
             print("--Especialidad Pollo--")
             run1 = True
+            print("")
+            print("¿Qué nivel de precios relativos estaria dispuesto a pagar?")
+            print("1. Alto (Q.71+) \n2. Medio (Q.51 - Q.70) \n3. Bajo(Q.1 - Q.50)")
+
+            run2 = False
+            while not run2:
+              op2 = pedirNumeroEntero()
+
+              if op2 == 1:
+                print("--Especialidad Pollo, Precio Alto--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Pollo, Precio Alto, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Pollo, Precio Alto, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+
+              elif op2 == 2:
+                print("--Especialidad Pollo, Precio Medio--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Pollo, Precio Medio, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Pollo, Precio Medio, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+
+              elif op2 == 3:
+                print("--Especialidad Pollo, Precio Bajo--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Pollo, Precio Bajo, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Pollo, Precio Bajo, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+              else:
+                print("\n Ingrese un numero entre 1 - 3 \n")
+
+
           elif op1 == 3:
             print("--Especialidad Hamburguesa--")
             run1 = True
+            print("")
+            print("¿Qué nivel de precios relativos estaria dispuesto a pagar?")
+            print("1. Alto (Q.71+) \n2. Medio (Q.51 - Q.70) \n3. Bajo(Q.1 - Q.50)")
+
+            run2 = False
+            while not run2:
+              op2 = pedirNumeroEntero()
+
+              if op2 == 1:
+                print("--Especialidad Hamburguesa, Precio Alto--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Hamburguesa, Precio Alto, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Hamburguesa, Precio Alto, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+
+              elif op2 == 2:
+                print("--Especialidad Hamburguesa, Precio Medio--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Hamburguesa, Precio Medio, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Hamburguesa, Precio Medio, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+
+              elif op2 == 3:
+                print("--Especialidad Hamburguesa, Precio Bajo--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Hamburguesa, Precio Bajo, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Hamburguesa, Precio Bajo, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+
+              else:
+                print("\n Ingrese un numero entre 1 - 3 \n")
+
           elif op1 == 4:
             print("--Especialidades Varias--")
             run1 = True
+            print("")
+            print("¿Qué nivel de precios relativos estaria dispuesto a pagar?")
+            print("1. Alto (Q.71+) \n2. Medio (Q.51 - Q.70) \n3. Bajo(Q.1 - Q.50)")
+
+            run2 = False
+            while not run2:
+              op2 = pedirNumeroEntero()
+
+              if op2 == 1:
+                print("--Especialidad Varias, Precio Alto--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Varias, Precio Alto, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Varias, Precio Alto, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+
+              elif op2 == 2:
+                print("--Especialidad Varias, Precio Medio--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Varias, Precio Medio, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Varias, Precio Medio, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+
+              elif op2 == 3:
+                print("--Especialidad Varias, Precio Bajo--")
+                run2 = True
+                print("")
+                print("¿En que Ambiente se sentiría mejor degustar de su comida?")
+                print("1. Ambiente Serio \n2. Ambiente Comida Rapida")
+                run3 = False
+                while not run3:
+                  op3 = pedirNumeroEntero()
+
+                  if op3 == 1:
+                    print("--Especialidad Varias, Precio Bajo, Ambiente Serio--")
+                    run3 = True
+                    run = True
+                  elif op3 == 2:
+                    print("--Especialidad Varias, Precio Bajo, Ambiente Comida Rápida--")
+                    run3 = True
+                    run = True
+                  else:
+                    print("\n Ingrese un numero entre 1 - 2 \n")
+
+
+              else:
+                print("\n Ingrese un numero entre 1 - 3 \n")
+
           else:
              print("\n Ingrese un numero entre 1 - 4 \n")
           
