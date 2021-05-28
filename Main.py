b0715dd8ffabc26dec60d9f538d96dae03e90aa1
Nate20029 <70552363+Nate20029@users.noreply.github.com>
2021-05-27 20:14:49 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,20 @@
 from DataBase import *
+import Usuarios as usuarios
 
 run = True
-
+usuarios.datos()
 while run:
-    print("Sistema de Recomendacion de Restaurantes")
+    
+    print("---------- Bienvenid@ ----------")
     print("1. Realizar una recomendacion\n2.Salir del Programa")
     op = input("Ingrese la opcion a realizar: ")
 
     # Empezar el con el sistema de recomendacion
     if op == "1":
+        print("")
         print("Procesando opciones\n")
 
     if op == "2":
+        print("")
         print("Gracias por utlizar el programa :)\n ")
         run = False
