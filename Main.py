ed82381b6d2ece3005806e09de0792fe2b97452f
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2021-05-27 21:25:30 -0600

    Update Main.py
    Primera pregunta para recomendar al usuario

--- Main.py
+++ Main.py
@@ -19,6 +19,9 @@ def pedirNumeroEntero():
  
 run = False
 op = 0
+op1 = 0
+op2 = 0
+op3 = 0
 
 while not run:
     
@@ -28,7 +31,29 @@ while not run:
     # Empezar el con el sistema de recomendacion
     if op == 1:
         print("")
-        print("Procesando opciones\n")
+        print("Instrucciones: A continuacion se le presentara un listado de preguntas, las cuales esperamos que responda segun sus antojos \n")
+        print("")
+        print("¿Qué especialidad de comida quisiera comer hoy?")
+        print("1. Pizza \n2. Pollo \n3. Hamburguesa \n4. Otros ")
+        run1 = False
+        while not run1:
+          op1 = pedirNumeroEntero()
+          if op1 == 1:
+            print("--Especialidad Pizza--")
+            run1 = True
+          elif op1 == 2:
+            print("--Especialidad Pollo--")
+            run1 = True
+          elif op1 == 3:
+            print("--Especialidad Hamburguesa--")
+            run1 = True
+          elif op1 == 4:
+            print("--Especialidades Varias--")
+            run1 = True
+          else:
+             print("\n Ingrese un numero entre 1 - 4 \n")
+          
+          
 
     elif op == 2:
         print("")
