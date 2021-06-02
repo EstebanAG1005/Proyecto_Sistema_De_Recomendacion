03cfec681e180aac62cc95701dad2a19519964cc
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2021-06-01 21:58:00 -0600

    Update Main.py
    Programación defensiva

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
-import Graph
-
+import Graph 
 run = True
 op1 = 0
 op2 = 0
@@ -20,17 +19,18 @@ def pedirNumeroEntero():
 
 op = 0
 
+
 while run:
 
     print("\nSistema de Recomendacion de Restaurantes")
     print("1.Realizar una recomendacion\n2.Agregar Informacion a la Base de Datos\n3.Eliminar "
           "informacion de la base de datos\n4.Salir del Programa")
-    op = int(input("Ingrese la opcion a realizar: "))
+    op = pedirNumeroEntero()
 
     # Empezar el con el sistema de recomendacion
     if op == 1:
         print(
-            "Instrucciones: A continuacion se le presentara un listado de preguntas, las cuales esperamos que "
+            "\nInstrucciones: A continuacion se le presentara un listado de preguntas, las cuales esperamos que "
             "responda segun sus antojos\n")
         esp = input("¿Qué especialidad de comida quisiera comer hoy? (Opciones: Pizza-Pollo-Hamburguesa-Otros(Ingrese "
                     "Lo que usted busca): ")
@@ -42,28 +42,37 @@ while run:
 
         Graph.Buscar(Especialidad, Precio, Ambiente)
 
-    if op == 2:
-        print("\n------Agragar Informacion a la base de datos----------")
+    elif op == 2:
+        print("\n------Agregar Informacion a la base de datos----------")
         nom = input("Ingrese el nombre del Restaurante que desea agregar: ")
         Nombre = nom.title()
-        Espe = input("Ingrese la Especialidad de este restaurante: ")
-        Especialidad = Espe.title()
+        Especialidad = ""
+        while Especialidad != ("Pizza"&&"Hamburguesa"&&"Pollo"&&"Otros") :
+          Espe = input("Ingrese la Especialidad de este restaurante: (Opciones: Pizza-Pollo-Hamburguesas-Otros)")
+          Especialidad = Espe.title()
         pe = input("Ingrese el Precio de promedio de este restaurante (Bajo)(Medio)(Alto): ")
         Precio = pe.title()
         am = input("Ingrese el Ambiente de este restaurante (Comida Rapida) (Serio) ")
         Ambiente = am.title()
 
         Graph.Agregar(Nombre, Especialidad, Precio, Ambiente)
-        Graph.Add(Nombre, Especialidad, Precio, Ambiente)
+        print("")
+        print("Informacion Agregada con Exito")
+        print("")
 
-    if op == 3:
+    elif op == 3:
         print("\n------Eliminar Informacion a la base de datos----------")
         Res = input('Ingrese el nombre del Restaurante que desea eliminar: ')
         Restaurante = Res.title()
 
         Graph.Borrar(Restaurante)
-        Graph.delete(Restaurante)
 
-    if op == 4:
+        print("")
+        print("Informacion Eliminada con Exito")
+        print("")
+
+    elif op == 4:
         print("\nGracias por utlizar el programa :)\n ")
         run = False
+    else:
+      print("\n Ingrese un numero entre 1 y 4 \n")
